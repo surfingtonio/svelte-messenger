f3d8e9d0a0d9dcf9ff218053df4549f2d88c8ac9
Marc Anthony Danting <marc@danting.ca>
2020-02-10 12:01:09 -0800

    Moved chat message to ChatMessage component.

---
+++ src/components/ChatMessage.svelte
@@ -0,0 +1,71 @@
+<script>
+  import moment from 'moment';
+
+  export let chat = {};
+  export let user = {};
+</script>
+
+<style>
+  .chat-message {
+    display: flex;
+  }
+
+  .chat-message .chat {
+    max-width: 500px;
+  }
+
+  .chat-message .username {
+    color: var(--component-secondary-color);
+    font-size: var(--component-small-font);
+  }
+
+  .chat-message .message {
+    background: var(--message-other-color);
+    border-radius: 0 var(--message-border-radius) var(--message-border-radius)
+      var(--message-border-radius);
+    padding: var(--message-padding-y) var(--message-padding-x);
+    word-break: break-word;
+  }
+
+  .chat-message .message .time {
+    color: var(--component-secondary-color);
+    display: block;
+    font-size: var(--component-small-font);
+    padding-top: 0.25rem;
+    text-align: right;
+  }
+
+  .chat-message.you {
+    justify-content: flex-end;
+  }
+
+  .chat-message.you .user {
+    text-align: right;
+  }
+
+  .chat-message.you .message {
+    background: var(--message-you-color);
+    border-radius: var(--message-border-radius) 0 var(--message-border-radius)
+      var(--message-border-radius);
+  }
+
+  @media (max-width: 600px) {
+    .chat-message .chat {
+      max-width: 100%;
+    }
+  }
+</style>
+
+<div class={chat.user.id === user.id ? 'chat-message you' : 'chat-message'}>
+  <div class="chat">
+    <div class="user">
+      <span class="username">
+        {chat.user.id === user.id ? 'You' : chat.user.username}
+      </span>
+    </div>
+    <div class="message">
+      <span class="content">{chat.message.content}</span>
+      <span class="time">{moment(chat.message.time).format('h:mm:ss a')}</span>
+    </div>
+  </div>
+</div>
--- src/components/ChatMessages.svelte
+++ src/components/ChatMessages.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount } from 'svelte';
-  import moment from 'moment';
+  import ChatMessage from './ChatMessage.svelte';
 
   export let user;
   export let chats = [];
@@ -16,7 +16,7 @@
 </script>
 
 <style>
-  .chat-items {
+  .chat-messages {
     flex-direction: column;
     display: flex;
     height: 100%;
@@ -24,49 +24,6 @@
     overflow-y: scroll;
     padding: var(--chat-item-padding-y) var(--chat-item-padding-x);
     width: calc(var(--chat-item-max-width) - (var(--chat-item-padding-x) * 2));
-  }
-
-  .chat-items .chat-item {
-    display: flex;
-  }
-
-  .chat-items .chat-item .chat {
-    max-width: 45%;
-  }
-
-  .chat-items .chat-item .username {
-    color: var(--component-secondary-color);
-    font-size: var(--component-small-font);
-  }
-
-  .chat-items .message {
-    background: var(--message-other-color);
-    border-radius: 0 var(--message-border-radius) var(--message-border-radius)
-      var(--message-border-radius);
-    padding: var(--message-padding-y) var(--message-padding-x);
-    word-break: break-word;
-  }
-
-  .chat-items .message .time {
-    color: var(--component-secondary-color);
-    display: block;
-    font-size: var(--component-small-font);
-    padding-top: 0.25rem;
-    text-align: right;
-  }
-
-  .chat-items .you {
-    justify-content: flex-end;
-  }
-
-  .chat-items .you .user {
-    text-align: right;
-  }
-
-  .chat-items .you .message {
-    background: var(--message-you-color);
-    border-radius: var(--message-border-radius) 0 var(--message-border-radius)
-      var(--message-border-radius);
   }
 
   ::-webkit-scrollbar {
@@ -89,22 +46,8 @@
   }
 </style>
 
-<div class="chat-items" bind:this={chatWindow}>
+<div class="chat-messages" bind:this={chatWindow}>
   {#each chats as chat}
-    <div class={chat.user.id === user.id ? 'chat-item you' : 'chat-item'}>
-      <div class="chat">
-        <div class="user">
-          <span class="username">
-            {chat.user.id === user.id ? 'You' : chat.user.username}
-          </span>
-        </div>
-        <div class="message">
-          <span class="content">{chat.message.content}</span>
-          <span class="time">
-            {moment(chat.message.time).format('h:mm:ss a')}
-          </span>
-        </div>
-      </div>
-    </div>
+    <ChatMessage {user} {chat} />
   {/each}
 </div>
